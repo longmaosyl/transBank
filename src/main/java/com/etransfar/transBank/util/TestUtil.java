f8bd4e76707a335c77531de14aa617b80edd1072
shiyulong <shiyl@51buding.cn>
2018-09-08 12:00:03 +0800

    测试类结尾打印

--- src/main/java/com/etransfar/transBank/util/TestUtil.java
+++ src/main/java/com/etransfar/transBank/util/TestUtil.java
@@ -5,6 +5,7 @@ public class TestUtil {
     public static void main(String[] args) {
         System.out.println("龙猫测试");
         System.out.println("aaaa");
+        System.out.println("龙猫测试end");
     }
 }
 
