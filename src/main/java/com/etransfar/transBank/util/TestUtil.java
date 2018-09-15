34539ea86406060d55b4088ec704e7c8b24fd5e4
shiyulong <shiyl@51buding.cn>
2018-09-15 16:26:10 +0800

    还原修改

--- src/main/java/com/etransfar/transBank/util/TestUtil.java
+++ src/main/java/com/etransfar/transBank/util/TestUtil.java
@@ -7,7 +7,7 @@ public class TestUtil {
         System.out.println("aaaa");
         System.out.println("龙猫测试end");
         System.out.println("第一次修改");
-        System.out.printf("第二次修改");
+        System.out.printf("第二次修改的");
     }
 }
 
